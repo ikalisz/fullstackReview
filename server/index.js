daac47fd22f4eb5110506446bd9cbb180043bddb
Ian Kaliszewski <ikalisz@yahoo.com>
2019-05-30 09:47:33 -0600

    set up main/proxy and server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const massive = require('massive')
+const session = require('express-session')
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+app.use(express.json())
+massive(CONNECTION_STRING)
+.then(db => {
+    app.set('db', db)
+    console.log('DB connected')
+})
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    }
+}))
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server working on port ${SERVER_PORT}`)
+})
